a432907615ed063504718ecdafa90c8bd60df79e
Ellen O'Neill <ellen.o.neill.munro@gmail.com>
2018-06-04 10:48:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector("#nested .target");
+}
+
+function increaseRankBy(n) {
+  var rankedLists = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0; i < rankedLists.length; i++) {
+    var children = rankedLists[i].children;
+
+    for (let j = 0; j < children.length; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
+  }
+}
+
+function deepestChild() {
+  var grandNode = document.querySelector('#grand-node');
+  var deepestChild = grandNode.querySelector('div').querySelector('div').querySelector('div').querySelector('div'); 
+  return deepestChild;
+}
