8a961a9b59855517c2b6284852b1624024d60ba8
Ellen O'Neill <ellen.o.neill.munro@gmail.com>
2018-06-04 10:29:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector("#nested .target");
+}
+
+function increaseRankBy(n) {
+  var rankedList = document.querySelectorAll("ul.ranked-list li");
+  for (let i = 0; i < rankedList.length; i++) {
+    rankedList[i].innerHTML = parseInt(i + 1);
+  }
+  //access .ranked-list
+  //loop through each item
+  //increase rank by n
+}
+
+function deepestChild() {
+  
+}
